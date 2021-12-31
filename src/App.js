669bd131f827595b987c952010c96026e278fc74
romjanxr <romjanvr5@gmail.com>
2021-12-31 18:22:36 +0600

    firebase functionality added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Home from './components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './contexts/AuthProvider';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
